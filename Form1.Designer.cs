b46010efd6c4be5e8be1cd2d2f7f8ea715e99faf
Paul <thepaulxxx@gmail.com>
2024-05-18 02:51:08 +0500

    forst upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BitRotateDecryptApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox rawHexTextBox;
+        private System.Windows.Forms.TextBox decryptedHexTextBox;
+        private System.Windows.Forms.NumericUpDown shiftNumericUpDown;
+        private System.Windows.Forms.Label rawHexLabel;
+        private System.Windows.Forms.Label decryptedHexLabel;
+        private System.Windows.Forms.Label shiftLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.rawHexTextBox = new System.Windows.Forms.TextBox();
+            this.decryptedHexTextBox = new System.Windows.Forms.TextBox();
+            this.shiftNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.rawHexLabel = new System.Windows.Forms.Label();
+            this.decryptedHexLabel = new System.Windows.Forms.Label();
+            this.shiftLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.shiftNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rawHexTextBox
+            // 
+            this.rawHexTextBox.Location = new System.Drawing.Point(12, 50);
+            this.rawHexTextBox.Multiline = true;
+            this.rawHexTextBox.Name = "rawHexTextBox";
+            this.rawHexTextBox.Size = new System.Drawing.Size(338, 207);
+            this.rawHexTextBox.TabIndex = 0;
+            this.rawHexTextBox.TextChanged += new System.EventHandler(this.rawHexTextBox_TextChanged);
+            // 
+            // decryptedHexTextBox
+            // 
+            this.decryptedHexTextBox.Location = new System.Drawing.Point(450, 50);
+            this.decryptedHexTextBox.Multiline = true;
+            this.decryptedHexTextBox.Name = "decryptedHexTextBox";
+            this.decryptedHexTextBox.Size = new System.Drawing.Size(338, 207);
+            this.decryptedHexTextBox.TabIndex = 1;
+            // 
+            // shiftNumericUpDown
+            // 
+            this.shiftNumericUpDown.Location = new System.Drawing.Point(356, 68);
+            this.shiftNumericUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.shiftNumericUpDown.Name = "shiftNumericUpDown";
+            this.shiftNumericUpDown.Size = new System.Drawing.Size(88, 20);
+            this.shiftNumericUpDown.TabIndex = 2;
+            this.shiftNumericUpDown.ValueChanged += new System.EventHandler(this.shiftNumericUpDown_ValueChanged);
+            // 
+            // rawHexLabel
+            // 
+            this.rawHexLabel.Location = new System.Drawing.Point(150, 20);
+            this.rawHexLabel.Name = "rawHexLabel";
+            this.rawHexLabel.Size = new System.Drawing.Size(100, 23);
+            this.rawHexLabel.TabIndex = 3;
+            this.rawHexLabel.Text = "Raw Hex";
+            // 
+            // decryptedHexLabel
+            // 
+            this.decryptedHexLabel.Location = new System.Drawing.Point(550, 20);
+            this.decryptedHexLabel.Name = "decryptedHexLabel";
+            this.decryptedHexLabel.Size = new System.Drawing.Size(100, 23);
+            this.decryptedHexLabel.TabIndex = 4;
+            this.decryptedHexLabel.Text = "Decrypted Hex";
+            // 
+            // shiftLabel
+            // 
+            this.shiftLabel.Location = new System.Drawing.Point(356, 50);
+            this.shiftLabel.Name = "shiftLabel";
+            this.shiftLabel.Size = new System.Drawing.Size(88, 15);
+            this.shiftLabel.TabIndex = 5;
+            this.shiftLabel.Text = "Shift";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 263);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 329);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 604);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.rawHexTextBox);
+            this.Controls.Add(this.decryptedHexTextBox);
+            this.Controls.Add(this.shiftNumericUpDown);
+            this.Controls.Add(this.rawHexLabel);
+            this.Controls.Add(this.decryptedHexLabel);
+            this.Controls.Add(this.shiftLabel);
+            this.Name = "Form1";
+            this.Text = "Bit Rotate Decrypt App";
+            ((System.ComponentModel.ISupportInitialize)(this.shiftNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BitRotateDecryptApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            shiftNumericUpDown.Minimum = 0;
+            shiftNumericUpDown.Maximum = 10;
+        }
+
+        private void shiftNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            DecryptText();
+        }
+
+        private void rawHexTextBox_TextChanged(object sender, EventArgs e)
+        {
+            DecryptText();
+        }
+
+        private void DecryptText()
+        {
+            string rawHex = rawHexTextBox.Text.Replace(" ", "");
+            byte[] rawBytes = StringToByteArray(rawHex);
+
+            int shiftAmount = (int)shiftNumericUpDown.Value;
+
+            byte[] decryptedBytes = new byte[rawBytes.Length];
+            Array.Copy(rawBytes, decryptedBytes, rawBytes.Length);
+
+            BitRotateDecrypt(decryptedBytes, decryptedBytes.Length, (byte)shiftAmount);
+
+            string decryptedHex = BitConverter.ToString(decryptedBytes).Replace("-", " ");
+            decryptedHexTextBox.Text = decryptedHex;
+
+            string decryptedHexDump = HexDump(decryptedBytes);
+            richTextBox1.Text = decryptedHexDump;
+        }
+
+        void BitRotateDecrypt(byte[] data, int length, byte shiftAmount)
+        {
+            byte previous = data[length - 1];
+
+            for (int i = length - 1; i >= 0; --i)
+            {
+                byte current = (byte)(i <= 0 ? previous : data[i - 1]);
+                byte shifted = (byte)(data[i] >> shiftAmount);
+                data[i] = (byte)((current << (8 - shiftAmount)) | shifted);
+                previous = data[i];
+            }
+        }
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            int numberChars = hex.Length;
+            byte[] bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i += 2)
+            {
+                string byteString = hex.Substring(i, 2);
+                bytes[i / 2] = Convert.ToByte(byteString, 16);
+            }
+            return bytes;
+        }
+
+
+        public static string HexDump(byte[] bytes, int bytesPerLine = 16)
+        {
+            if (bytes == null) return "<null>";
+            int bytesLength = bytes.Length;
+
+            char[] HexChars = "0123456789ABCDEF".ToCharArray();
+
+            int firstHexColumn =
+                  8                   // 8 characters for the address
+                + 3;                  // 3 spaces
+
+            int firstCharColumn = firstHexColumn
+                + bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
+                + (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
+                + 2;                  // 2 spaces 
+
+            int lineLength = firstCharColumn
+                + bytesPerLine           // - characters to show the ascii value
+                + Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
+
+            char[] line = (new String(' ', lineLength - 2) + Environment.NewLine).ToCharArray();
+            int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
+            StringBuilder result = new StringBuilder(expectedLines * lineLength);
+
+            for (int i = 0; i < bytesLength; i += bytesPerLine)
+            {
+                line[0] = HexChars[(i >> 28) & 0xF];
+                line[1] = HexChars[(i >> 24) & 0xF];
+                line[2] = HexChars[(i >> 20) & 0xF];
+                line[3] = HexChars[(i >> 16) & 0xF];
+                line[4] = HexChars[(i >> 12) & 0xF];
+                line[5] = HexChars[(i >> 8) & 0xF];
+                line[6] = HexChars[(i >> 4) & 0xF];
+                line[7] = HexChars[(i >> 0) & 0xF];
+
+                int hexColumn = firstHexColumn;
+                int charColumn = firstCharColumn;
+
+                for (int j = 0; j < bytesPerLine; j++)
+                {
+                    if (j > 0 && (j & 7) == 0) hexColumn++;
+                    if (i + j >= bytesLength)
+                    {
+                        line[hexColumn] = ' ';
+                        line[hexColumn + 1] = ' ';
+                        line[charColumn] = ' ';
+                    }
+                    else
+                    {
+                        byte b = bytes[i + j];
+                        line[hexColumn] = HexChars[(b >> 4) & 0xF];
+                        line[hexColumn + 1] = HexChars[b & 0xF];
+                        line[charColumn] = (b < 32 ? '·' : (char)b);
+                    }
+                    hexColumn += 3;
+                    charColumn++;
+                }
+                result.Append(line);
+            }
+            return result.ToString();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            DecryptText();
+        }
+    }
+}
